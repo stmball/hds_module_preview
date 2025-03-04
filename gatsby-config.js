0103eb68263fd278741dd20292b70af4fdca32be
stmball <stmball@gmail.com>
2025-03-04 14:08:31 +0000

    backtikcs:

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
   siteMetadata: {
     title: `University of Liverpool Health Data Science Modules`,
   },
-  pathPrefix: "/hds_module_preview",
+  pathPrefix: `/hds_module_preview`,
   plugins: [
     `gatsby-plugin-image`,
     {
